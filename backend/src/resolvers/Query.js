fedfde3be32a2bb6b9667800d9ae19b31a7c39d6
imdbsd <budisuryadarma0407@gmail.com>
2018-11-12 07:44:44 +0800

    add signin

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,17 @@ const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info){
+        console.log({minta: ctx.request.userId})
+        if(!ctx.request.userId){
+            return null;
+        }
+        return ctx.db.query.user({
+            where: {
+                id: ctx.request.userId
+            }
+        }, info);
+    }
     // async items(parent, args, ctx, info){
     //     const items = await ctx.db.query.items();
     //     return items;
